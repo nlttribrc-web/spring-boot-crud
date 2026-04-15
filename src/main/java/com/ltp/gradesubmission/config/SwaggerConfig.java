e6e8f7474a4fd5d330b2c84b2c04029998a3c9ec
Tri Nguyen <ntri1104@gmail.com>
2026-04-15 19:58:07 +0700

    minor change

--- src/main/java/com/ltp/gradesubmission/config/SwaggerConfig.java
+++ src/main/java/com/ltp/gradesubmission/config/SwaggerConfig.java
@@ -16,7 +16,6 @@ public class SwaggerConfig {
 	public OpenAPI customOpenAPI() {
 		
 		return new OpenAPI()
-				.info(new Info().title("JavaInUse Authentication Service"))				
 				.addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
 				.components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
 						.name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));

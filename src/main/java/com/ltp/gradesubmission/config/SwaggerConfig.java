6338da3b455be2a4771e902875a4db8faea12a7c
Tri Nguyen <ntri1104@gmail.com>
2026-04-15 19:58:36 +0700

    remove import

--- src/main/java/com/ltp/gradesubmission/config/SwaggerConfig.java
+++ src/main/java/com/ltp/gradesubmission/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
